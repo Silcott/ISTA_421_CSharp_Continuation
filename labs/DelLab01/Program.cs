6889a05fcec4535ea286ba08b17846b4152ebc84
JBSilcott <silcott.jb@gmail.com>
2020-10-09 13:34:12 -0400

    update

---
+++ labs/DelLab01/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace DelLab01
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lab1
+            Console.WriteLine("LAB 1");
+            Lab1.RunLab1();
+            Console.WriteLine("-------------------------------------------------\n");
+
+            //Lab2
+            Console.WriteLine("LAB 2");
+            Lab2.RunLab2();
+            Console.WriteLine("-------------------------------------------------\n");
+
+            //Lab3
+            Console.WriteLine("LAB 3");
+            Lab3.RunLab3();
+            Console.WriteLine("-------------------------------------------------\n");
+
+        }
+        public static class Lab1
+        {
+            //1. declare a delegate type
+            delegate void MyDel();//can name it anthing
+
+            public static void RunLab1()
+            {
+                Console.WriteLine("Delegate Lab 1");
+                //2. instantiate a delegate object
+                //either of the two lines below, a or b
+                //var md = new MyDel(someMethod); //a 
+                //MyDel md = someMethod; //b
+                MyDel md = MomsName;
+                //md = someMethod;
+                //3. call the delegate
+                md();
+
+                md += someMethod;
+                md += MomsName;
+                md.Invoke();
+            }
+            public static void someMethod()
+            {
+                Console.WriteLine("This is the body of the method named \"someMethod()\"");
+            }
+            public static void MomsName()
+            {
+                int a = 5;
+                int b = 6;
+                int c = a * b;
+                Console.WriteLine($"The value of {a} times {b} equals {c}");
+            }
+        }
+        public static class Lab2
+        {
+            delegate int IntDel(int a, int b);
+
+            static int Add(int a, int b) => a + b;
+            static int Sub(int a, int b) => a - b;
+            static int Prod(int a, int b) => a * b;
+            static int Mod(int a, int b) => a % b;
+            static int Pow(int a, int b) => (int)Math.Pow(a, b);
+
+            private static (int a, int b) GetInput()
+            {
+                Console.WriteLine("Enter an integer for the left hand side");
+                int a, b;
+                int.TryParse(Console.ReadLine(), out a);
+                Console.WriteLine("Enter an integer for the right hand side");
+                int.TryParse(Console.ReadLine(), out b);
+                return (a, b);
+            }
+            public static void RunLab2()
+            {
+                Console.WriteLine("Hello Del20200327c!");
+                (int a, int b) = GetInput();
+
+                Console.WriteLine("=======using a method call=========");
+                int result = Add(a, b);
+                Console.WriteLine(result);
+                result = Sub(a, b);
+                Console.WriteLine(result);
+                result = Prod(a, b);
+                Console.WriteLine(result);
+                result = Mod(a, b);
+                Console.WriteLine(result);
+                result = Pow(a, b);
+                Console.WriteLine(result);
+
+                Console.WriteLine("=======using a delegate=========");
+                IntDel idel = new IntDel(Add);
+                Console.WriteLine(idel(a, b));
+                idel = Sub;
+                Console.WriteLine(idel(a, b));
+                idel = Prod;
+                Console.WriteLine(idel(a, b));
+                idel = Mod;
+                Console.WriteLine(idel(a, b));
+                idel = Pow;
+                Console.WriteLine(idel(a, b));
+            }
+        }
+        public class President
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string State { get; set; }
+            public string Party { get; set; }
+            public int Year { get; set; }
+
+            public override string ToString()
+            {
+                return $"{FirstName} {LastName} from {State} was a {Party} elected in {Year}.";
+            }
+
+            //Get takes no arguments ahd returns a string
+            public string GetFirstName() => FirstName;
+            public string GetLastName() => LastName;
+            public string GetState() => State;
+            public string GetParty() => Party;
+            public string GetYear() => Year.ToString();
+
+            //print takes a President argument and returns void
+            public void PrintFirstName(President p) => Console.WriteLine(p.FirstName);
+            public void PrintLastName(President p) => Console.WriteLine(p.LastName);
+            public void PrintState(President p) => Console.WriteLine(p.State);
+            public void PrintParty(President p) => Console.WriteLine(p.Party);
+            public void PrintYear(President p) => Console.WriteLine(p.Year.ToString());
+        }
+        public static class Lab3
+        {
+            public delegate void PrintPresDel(President p);
+            public delegate string GetPresDel();
+            public static void RunLab3()
+            {
+                Console.WriteLine("Hello Delegates!");
+                President first = new President
+                {
+                    FirstName = "George",
+                    LastName = "Washington",
+                    State = "Virginia",
+                    Party = "none",
+                    Year = 1788
+                };
+                Console.WriteLine("=========non delegate demostration =========");
+                Console.WriteLine(first.ToString());
+                first.PrintFirstName(first);
+                Console.WriteLine(first.GetFirstName());
+                Console.WriteLine(first.FirstName);
+                Console.WriteLine(first.FirstName.GetType());
+
+                Console.WriteLine("=========first delegate demostration =========");
+                GetPresDel myFirstDel = new GetPresDel(first.GetFirstName);
+                Console.WriteLine(myFirstDel());
+                myFirstDel = first.GetLastName;
+                Console.WriteLine(myFirstDel());
+                myFirstDel = first.GetState;
+                Console.WriteLine(myFirstDel());
+                myFirstDel = first.GetParty;
+                Console.WriteLine(myFirstDel());
+                myFirstDel = first.GetYear;
+                Console.WriteLine(myFirstDel());
+
+                Console.WriteLine("=========second delegate demostration =========");
+                PrintPresDel mySecondDel = new PrintPresDel(first.PrintFirstName);
+                mySecondDel(first);
+                mySecondDel = new PrintPresDel(first.PrintLastName);
+                mySecondDel(first);
+                mySecondDel = new PrintPresDel(first.PrintState);
+                mySecondDel(first);
+                mySecondDel = new PrintPresDel(first.PrintParty);
+                mySecondDel(first);
+                mySecondDel = new PrintPresDel(first.PrintYear);
+                mySecondDel(first);
+            }
+        }
+
+    }
+}
---
+++ quizzes/Quiz20_Delegates/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Quiz20_Delegates
+{
+    delegate void MyDel();
+    class Program
+    {
+        public class Rifle
+        {
+            public static void Shoot()
+            {
+                Console.WriteLine("I am a Rifle and I go Bang.");
+            }
+        }
+        public class Shotgun
+        {
+            public static void Shoot()
+            {
+                Console.WriteLine("I am a Shotgun and I go Boom.");
+            }
+        }
+        public class Pistol
+        {
+            public static void Shoot()
+            {
+                Console.WriteLine($"I am a Pistol and I go Pop.");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is C# chapter 20 quiz");
+            MyDel del1, del2, del3, del4;
+            del1 = Rifle.Shoot;
+            del2 = Shotgun.Shoot;
+            del3 = Pistol.Shoot;
+
+            del4 = del1 + del2 + del3;
+
+            del4();
+        }
+    }
+}
+
+//Shoot del = r.Speak;
+//del += p.Speak;
+//speak method
+//void method that print I am a X and I go Y.
+//I am a shotgun and I go Boom
+//I am a rifle and I go Bang
+//I am a pistol and I go Pop
+
+//Quiz 20 — Delegates
+//C# Programming
+//This is a timed test. You have thirty minutes to complete the test. When you finish the test, upload your
+//Program.cs to Canvas. Do not publish your answer to your git repository.
+//Create a console application. Create three classes, a Rifle class, a Pistol class, and a Shotgun class. In each
+//class, create one method, a void method that takes no parameters, and prints a message to console similar to this:
+//“I am a X and I go Y.” Repplace X and Y with the appropriate values. Create a delegate of the appropriate type.
+//Instantiate three objects and assign to the delegate the three methods.Call the delegate. Note that one call to the
+//delegate invokes three methods.Your output should be similar to that shown below.
+//Here is the expected output.
+//This is C Sharp quiz 20
+//I am a shotgun and I go Boom
+//I am a rifle and I go Bang
+//I am a pistol and I go Pop
+//Press any key to continue . . .
